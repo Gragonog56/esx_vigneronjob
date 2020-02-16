ae13219d93416d6d2e1ebe9c58cecb3c7a9c67a2
Gragonog56 <56315759+Gragonog56@users.noreply.github.com>
2020-02-16 13:43:21 +0200

    Add files via upload

---
+++ esx_vigneronjob/locales/en.lua
@@ -0,0 +1,47 @@
+-----------------------------------------
+-- Created and modify by L'ile Légale RP
+-- SenSi and Kaminosekai
+-----------------------------------------
+Locales['en'] = {
+	-- General
+	['cloakroom'] = 'Vestiário',
+	['vine_clothes_civil'] = 'Roupa Civil',
+	['vine_clothes_vine'] = 'Roupa de Trabalho',
+	['veh_menu'] = 'Garagem',
+	['not_enough_place'] = 'Não tens espaço suficiente',
+	['sale_in_prog'] = 'Venda a decorrer...',
+	['van'] = 'Carro de Trabalho',
+	['open_menu'] = ' ',
+	['spawn_veh'] = 'Carregue ~INPUT_CONTEXT~ para tirar o veículo',
+	['amount_invalid'] = 'Quantidade inválida',
+	['press_to_open'] = 'Carregue ~INPUT_CONTEXT~ para acessar ao menu',
+	['billing'] = 'Faturas',
+	['invoice_amount'] = 'Valor Fatura',
+	['no_players_near'] = 'sem jogadores perto',
+	['store_veh'] = 'Carrregue ~INPUT_CONTEXT~ para guardar veículo',
+	['comp_earned'] = 'A empresa ganhou ~g~€',
+	['deposit_stock'] = 'Depositar Stock',
+	['take_stock'] = 'Tirar Stock',
+	['boss_actions'] = 'Menu Patrão',
+	['quantity'] = 'quantidade',
+	['quantity_invalid'] = 'quantidade inválida',
+	['inventory'] = 'inventário',
+	['have_withdrawn'] = 'tiraste x',
+	['added'] = 'puseste x',
+	
+	-- Depend to the company
+	['vigneron_client'] = 'Cliente',
+	['transforming_in_progress'] = 'TTransformação das uvas em progresso ... ',
+	['raisin_taken'] = 'Estás a apanhar uvas',
+	['press_traitement'] = 'Carregue ~INPUT_CONTEXT~ para transformar uvas',
+	['press_collect'] = 'Carregue ~INPUT_CONTEXT~ para recolher uvas',
+	['press_sell'] = 'Carregue ~INPUT_CONTEXT~ para vender vinho',
+	['no_jus_sale'] = 'Não tens suco de uva suficiente',
+	['no_vin_sale'] = 'Não tens vinho suficiente',
+	['not_enough_raisin'] = 'Não tens uvas suficientes',
+	['grand_cru'] = '~g~ Uma garrafa vintage!~w~',
+	['no_product_sale'] = 'Não tens produtos suficientes',
+	['press_traitement_jus'] = 'Carregue ~INPUT_CONTEXT~ para transformar uvas',
+	['used_jus'] = 'Bebeste uma garrafa de vinho',
+	['used_grand_cru'] = 'Bebeste uma garrafa de vinho vintage',
+}
